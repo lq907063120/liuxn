c08b7ac3e88dbb06cf86bb00cd206d1389a3445f
xiaonanliu <lq907063120@163.com>
2018-01-11 21:18:26 +0800

    完善sql 默认为null

--- doc/sql/001-系统登陆.sql
+++ doc/sql/001-系统登陆.sql
@@ -1,15 +1,34 @@
-drop table if exists cmsUser;
+/*
+ Navicat Premium Data Transfer
 
-/*==============================================================*/
-/* Table: cmsUser                                               */
-/*==============================================================*/
-create table cmsUser
-(
-   id                   int not null auto_increment comment '表唯一主键自增',
-   vcloginname          varchar(32),
-   vcloginpwd           varchar(32),
-   datcreate            timestamp,
-   datlasttime          timestamp,
-   datupdate            timestamp,
-   primary key (id)
-);
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50720
+ Source Host           : localhost:3306
+ Source Schema         : liuxn
+
+ Target Server Type    : MySQL
+ Target Server Version : 50720
+ File Encoding         : 65001
+
+ Date: 11/01/2018 21:09:28
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for cmsUser
+-- ----------------------------
+DROP TABLE IF EXISTS `cmsUser`;
+CREATE TABLE `cmsUser` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `vcloginname` varchar(32) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `vcloginpwd` varchar(32) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `datcreate` timestamp NULL DEFAULT NULL,
+  `datlasttime` timestamp NULL DEFAULT NULL,
+  `datupdate` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
