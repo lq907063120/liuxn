0ba97618fd957a72033cfa22deffcf981caa4798
lq907063120 <lq907063120@163.com>
2018-01-12 19:06:43 +0800

    service 完善

--- liuxn-persist/src/test/java/com/liuxn/entity/raw/mapper/CmsUserMapperTest.java
+++ liuxn-persist/src/test/java/com/liuxn/entity/raw/mapper/CmsUserMapperTest.java
@@ -2,6 +2,7 @@ package com.liuxn.entity.raw.mapper;
 
 
 import com.liuxn.entity.raw.model.CmsUser;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
